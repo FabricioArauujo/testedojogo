a1f193711190412584a950b80b4dd18dc002dad6
wermesson-silva <wermesson.pessoal@gmail.com>
2024-03-05 11:34:05 -0300

    Adição de protótipo

---
+++ CenaTeste/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    public float velocidade;
+    public float stoppingDistance;
+    private Transform alvo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alvo = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Vector2.Distance(transform.position, alvo.position) < stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, alvo.position, velocidade * Time.deltaTime);
+        }
+    }
+}
---
+++ CenaTeste/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+{
+
+    public float velocidade = 5f;
+    public Rigidbody2D playerRb;
+    public Animator animator;
+
+    Vector2 movimento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimento.x = Input.GetAxisRaw("Horizontal");
+        movimento.y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("horizontal", movimento.x);
+        animator.SetFloat("vertical", movimento.y);
+        animator.SetFloat("velocidade", movimento.sqrMagnitude);
+
+        if(movimento != Vector2.zero)
+        {
+
+            animator.SetFloat("horizontalIdle", movimento.x);
+            animator.SetFloat("verticalIdle", movimento.y);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.MovePosition(playerRb.position + movimento * velocidade * Time.fixedDeltaTime);
+    }
+}
