8ccb0fe82e75e900c8d2e5537234d8c7b5b175f3
wermesson-silva <wermesson.pessoal@gmail.com>
2024-03-26 15:49:30 -0300

    atualizando diálogo

---
+++ CenaTeste/Assets/Scripts/Dialogo/Dialogue.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public Sprite profile;
+    public string[] speechText;
+    public string actorName;
+    private bool estaAberto;
+
+    public LayerMask playerLayer;
+    public float radius;
+
+    private DialogueControl dc;
+    bool onRadious;
+
+    private void Start()
+    {
+        dc = FindObjectOfType<DialogueControl>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onRadious && !estaAberto)
+        {
+            dc.Speech(profile, speechText, actorName);
+            estaAberto = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Interact();
+    }
+
+    public void Interact()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, radius, playerLayer);
+
+        if(hit != null)
+        {
+            onRadious = true;
+        }
+        else
+        {
+            onRadious = false;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
